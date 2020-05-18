0085c6d7299c61f5822078fc980fc4acf2a54b52
RUBEN DARIO ZIPA HERNANDEZ <rubenchotr1629@gmail.com>
2020-05-18 13:00:51 -0500

    componente del buscador

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { 
+
+  }
 
   ngOnInit(){
   }
   buscarHeroe(termino:string){
-    console.log(termino);
+    // console.log(termino);
+    this.router.navigate(['/buscar',termino]);
   }
 
 }
